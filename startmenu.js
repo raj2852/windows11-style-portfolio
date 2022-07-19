9f1fba87c4e1c337ff787a16d35fdd81aea15a8e
raj2852 <microsoftrajdip@gmail.com>
2022-07-20 00:03:27 +0530

    static date-time issue fixed

--- startmenu.js
+++ startmenu.js
@@ -13,12 +13,20 @@ start.addEventListener("click", ()=>{
 })
 
 /*fetch current date and time of location*/
-var h = new Date().getHours();
-var m = new Date().getMinutes();
-var time = h +":" +m;
-var d = new Date().getDate();
-var month = new Date().getMonth();
-var year = new Date().getFullYear();
-var date = d+"-"+month+"-"+year;
-document.getElementById("date").innerHTML=date;
-document.getElementById("time").innerHTML = time;
+// var h = new Date().getHours();
+// var m = new Date().getMinutes();
+// var time = h +":" +m;
+// var d = new Date().getDate();
+// var month = new Date().getMonth();
+// var year = new Date().getFullYear();
+// var date = d+"-"+month+"-"+year;
+// document.getElementById("date").innerHTML=date;
+// document.getElementById("time").innerHTML = time;
+
+function updateTime() {
+  var currentTime = new Date().toLocaleTimeString();
+  var currentDate = new Date().toLocaleDateString();
+  document.getElementById("date").innerHTML = currentDate;
+  document.getElementById("time").innerHTML = currentTime;
+}
+setInterval(updateTime, 1000);
